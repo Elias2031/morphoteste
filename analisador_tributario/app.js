59967a0d88ddc34495f043f0c698439294979fff
Elias <eliasjunior292001@gmail.com>
2024-12-12 00:16:35 -0300

    Configurando cors

--- analisador_tributario/app.js
+++ analisador_tributario/app.js
@@ -1,5 +1,5 @@
 // URL base do backend
-const API_BASE_URL = "https://analisador-tributario-vercel-577rbskg7-elias-silvas-projects.vercel.app";
+const API_BASE_URL = "https://analisador-tributario-vercel-axty3rtcs-elias-silvas-projects.vercel.app";
 let isProcessing = false; // Variável para rastrear o status de processamento
 let downloadUrl = null; // URL do arquivo gerado
 
